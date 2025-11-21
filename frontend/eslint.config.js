6b3f5ef0b0f6e308ceea57e34c0d41d158f55152
Rishiraj Dhar <dhar.ri@northeastern.edu>
2025-11-20 22:49:02 -0500

    build(ui): setup prettier

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 import pluginReact from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import { defineConfig, globalIgnores } from "eslint/config";
@@ -10,5 +11,6 @@ export default defineConfig([
   tseslint.configs.recommended,
   {...pluginReact.configs.flat.recommended, settings: {react: {version: "detect"}}},
   {...reactHooks.configs.flat.recommended, rules: {"react/react-in-jsx-scope": "off"}},
+  eslintConfigPrettier,
   globalIgnores([".react-router/", "build/**/*", "node_modules/"]),
 ]);
