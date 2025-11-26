df272a1d20a67aebd98bc3c175c3b87af332b774
Bhoomika <bhoomig252@gmail.com>
2025-11-26 05:05:02 -0500

    add database object for storing user's booking summary data and payload for filing a complaint

---
+++ backend/src/main/java/edu/northeastern/dharrguptab/huddleup/user/dto/BookingSummary.java
@@ -0,0 +1,36 @@
+package edu.northeastern.dharrguptab.huddleup.user.dto;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+/**
+ * Represents a summary of a booking for a user.
+ *
+ * @param bookingId the ID of the booking
+ * @param startTimeUtc the start time of the booking in UTC
+ * @param durationMins the duration of the booking in minutes
+ * @param amount the amount paid to confirm the booking
+ * @param complaintSubject the subject of the associated complaint if any
+ * @param complaintDescription the description of the associated complaint if any
+ * @param complaintFiledAtUtc the timestamp for when any existing complaint was filed
+ * @param complaintResolvedAtUtc the timestamp for when any existing complaint was resolved
+ * @param turfId the ID of the turf that was booked
+ * @param username the username of the user who made the booking
+ * @param maskedCardNumber the masked number of the card used to confirm the booking
+ * @param couponId the ID of the coupon applied to the booking if any
+ */
+public record BookingSummary(
+    int bookingId,
+    Instant startTimeUtc,
+    int durationMins,
+    BigDecimal amount,
+    String complaintSubject,
+    String complaintDescription,
+    Instant complaintFiledAtUtc,
+    Instant complaintResolvedAtUtc,
+    int turfId,
+    String username,
+    String maskedCardNumber,
+    Integer couponId
+) {}
+
