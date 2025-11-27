4b5359dd278296909f36d1f54f96f8c1e994a493
Rishiraj Dhar <dhar.ri@northeastern.edu>
2025-11-27 08:24:45 -0500

    feat(ui): add browse turfs route

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,3 +1,15 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  route("login", "routes/login.tsx"),
+  layout("routes/layout.tsx", [
+    // add protected routes here
+  ]),
+  route("browse", "routes/turf/browse-turfs.tsx"),
+] satisfies RouteConfig;
