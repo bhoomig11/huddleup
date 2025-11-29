a97b272a05da68233da43c381e0b770beb39a937
Rishiraj Dhar <dhar.ri@northeastern.edu>
2025-11-28 19:19:50 -0500

    feat(api): configure cors for the api

--- backend/src/main/java/edu/northeastern/dharrguptab/huddleup/auth/config/SecurityConfig.java
+++ backend/src/main/java/edu/northeastern/dharrguptab/huddleup/auth/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package edu.northeastern.dharrguptab.huddleup.auth.config;
 
 import edu.northeastern.dharrguptab.huddleup.auth.jwt.UserJwtFilter;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +28,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.csrf(csrf -> csrf.disable())
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
         .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(
             auth ->
@@ -45,5 +50,19 @@ public class SecurityConfig {
         .exceptionHandling(ex -> ex.authenticationEntryPoint(authEntryPoint));
 
     return http.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+    configuration.setAllowedMethods(List.of("*"));
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/api/**", configuration);
+    return source;
   }
 }
